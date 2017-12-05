81b28855ca611515d1b86aba67cf43c3c309c4fd
michaelujkim <michaelujkim@gmail.com>
2017-12-05 15:15:04 -0800

    add header text for front page.

--- themes/sosnpr/front-page.php
+++ themes/sosnpr/front-page.php
@@ -9,7 +9,9 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+  <h1> <span class="bold-title-span">South Okanagan SimilKameen</span> National Park</h1>
+  <h2 class="front-page-blurb">A place where biodiversity is preserved for future generations to enjoy<h2>
+  <button class="learn-more-button">Learn More</button>
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
