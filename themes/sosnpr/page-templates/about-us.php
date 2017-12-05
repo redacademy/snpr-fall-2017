409de9d683d18c27a699f4f48c3cdfe18dbfac99
qingxin-yuan <qingxin.yn@gmail.com>
2017-12-05 12:07:10 -0800

    Create page template files of about us and proposed park pages.

---
+++ themes/sosnpr/page-templates/about-us.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+* Template Name: About Us Page
+*
+*/
+get_header();
+?>
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+    <article id="post-<?php the_ID();?>" <?php post_class();?>>
+      <header class="entry-header custom-hero">
+        <div class="container">
+          <?php the_title('<h1 class="entry-title">','</h1>');?>
+        </div>
+      </header>
+      <div class="container">
+        
+      </div>
+
+    </article>
+
+  </main><!-- #main -->
+
+</div><!-- #primary -->
+<?php get_footer();?>
