02af9d29e0d41ed5ba30527e0070cdeaa0788fc2
michaelujkim <michaelujkim@gmail.com>
2017-12-06 15:32:07 -0800

    styled header

--- themes/sosnpr/js/menu-toggle.js
+++ themes/sosnpr/js/menu-toggle.js
@@ -1,20 +1,38 @@
 (function ($) {
   
-    $('.menu-toggle').on('click', function (e) {
-      e.preventDefault();
+    // $('.menu-toggle').on('click', function (e) {
+    //   e.preventDefault();
   
-      $(".menu").toggleClass("menu-show").focus();
-  
+    //   $(".menu").toggleClass("menu-show").focus();
   
       
-  
+    // })
+    $('.menu-toggle').click(function() {
+      
+      
+          $('.menu').toggleClass('menu-show');
+          // Remove the bind as it will be bound again on the next span click
+          $('.menu').unbind('click');
+      
+     
+    
+    });
+
+    
+    
+    $('.site-content').click(function(e) {
+      // If the click was not inside the active span
+      if(!$(e).hasClass("menu-show")) {
+          $('.menu').removeClass('menu-show');
+          // Remove the bind as it will be bound again on the next span click
+          $('.menu').unbind('click');
+      }
+      
+     
+    
     })
-    ;
-  
-    // $('.menu-toggle').on('blur', function(){
-    //   $(".menu").toggleClass("menu-show");
-  
-    // });
+   
+    
 
 
   
