2c85819f0f0120d075c9d2129b5219495644a2e6
qingxin-yuan <qingxin.yn@gmail.com>
2017-12-05 16:45:50 -0800

    Start pulling content on about us page.

--- themes/sosnpr/page-templates/about-us.php
+++ themes/sosnpr/page-templates/about-us.php
@@ -10,11 +10,27 @@ get_header();
     <article id="post-<?php the_ID();?>" <?php post_class();?>>
       <header class="entry-header custom-hero">
         <div class="container">
-          <?php the_title('<h1 class="entry-title">','</h1>');?>
+          <?php while ( have_posts() ) : the_post(); ?>
+
+			  	<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+		    	<?php endwhile; // End of the loop. ?>
         </div>
       </header>
       <div class="container">
-        
+      <?php 
+      // $props = CFS()->get_field_info( 'ssosnp_network' );
+      //     echo $props['label'];
+      //     echo CFS()->get('ssosnp_network');
+
+
+      $fields = CFS()->get( 'about_us_fields' );
+       d($fields);
+      // foreach( $fields as $field ) {
+      //     var_dump($field) ;
+          // echo $field['upload'];
+      // }
+      ?>
       </div>
 
     </article>
