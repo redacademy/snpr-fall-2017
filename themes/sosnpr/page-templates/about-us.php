117bed3282e094d9ed67336a8d0b0163a19efaac
qingxin-yuan <qingxin.yn@gmail.com>
2017-12-07 11:48:24 -0800

    Add section style in mobile and tablet versions on the about us page; add page navigation as well as smooth scroll functionality.

--- themes/sosnpr/page-templates/about-us.php
+++ themes/sosnpr/page-templates/about-us.php
@@ -18,17 +18,38 @@ get_header();
        
       </header>
 
+      <ul class="about-us-page-navigation">
+        <li>
+          <a href="#sosnp-network">s.o.s.n.p. network</a>
+        </li>
+        <li>
+          <a href="#first-nation-support">first nation support</a>
+        </li>
+        <li>
+          <a href="#community-support"> community support</a>
+        </li>
+        <li>
+          <a href="#government-support">government support</a>
+        </li>
+        <li>
+          <a href="#parks-canada-support">parks canada support</a>
+        </li>
+        <li>
+          <a href="#proposal-history">proposal history</a>
+        </li>
+      </ul>
+
       <ul class="about-us-content-wrapper">
       
-        <li>
+        <li id="sosnp-network">
           <?php $props = CFS()->get_field_info( 'ssosnp_network' );?> 
-          <h2 class="about-us-heading"><?php echo $props['label'];?></h2>
+          <h2 class="about-us-heading" ><?php echo $props['label'];?></h2>
           <div class="about-us-essay-hide">
             <p><?php echo CFS()->get('ssosnp_network');?></p>
           </div>
         </li>
 
-        <li>
+        <li id="first-nation-support">
           <?php $props = CFS()->get_field_info( 'first_nation_support' );?> 
           <h2 class="about-us-heading"><?php echo $props['label'];?></h2>
           <div class="about-us-essay-hide">
@@ -36,7 +57,7 @@ get_header();
           </div>
         </li>
 
-        <li>
+        <li id="community-support">
           <?php $props = CFS()->get_field_info( 'community_support' );?> 
           <h2 class="about-us-heading"><?php echo $props['label'];?></h2>
           <div class="about-us-essay-hide">
@@ -44,7 +65,7 @@ get_header();
           </div>
         </li>
 
-        <li>
+        <li id="government-support">
           <?php $props = CFS()->get_field_info( 'provincial_government_support' );?> 
           <h2 class="about-us-heading"><?php echo $props['label'];?></h2>
           <div class="about-us-essay-hide">
@@ -52,7 +73,7 @@ get_header();
           </div>
         </li>
 
-        <li>
+        <li id="parks-canada-support">
           <?php $props = CFS()->get_field_info( 'federal_government_parks_canada_support' );?> 
           <h2 class="about-us-heading"><?php echo $props['label'];?></h2>
           <div class="about-us-essay-hide">
@@ -60,7 +81,7 @@ get_header();
           </div>
         </li>
 
-        <li>
+        <li id="proposal-history">
           <?php $props = CFS()->get_field_info( 'proposal_history' );?> 
           <h2 class="about-us-heading"><?php echo $props['label'];?></h2>
           <div class="about-us-essay-hide">
