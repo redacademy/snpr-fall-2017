309c675ac7037b6fa0a1d53d26403b1e89dd37e2
michaelujkim <michaelujkim@gmail.com>
2017-12-11 11:16:03 -0800

    styled nav bar in single page

--- themes/sosnpr/single.php
+++ themes/sosnpr/single.php
@@ -7,23 +7,24 @@
 
 get_header(); ?>
 
+<div class="single-hero"><h1 class="single-header">Recent Updates</h1>
+</div>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			
+			<?php 
+			the_post_navigation(); 
+			?> 
 
-			<?php the_post_navigation(); ?>
+		
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
 
 		<?php endwhile; // End of the loop. ?>
+		
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
