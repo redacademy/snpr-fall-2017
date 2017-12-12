049b4e3f7b8c4758616a30edc7d613d59b02cc62
qingxin-yuan <qingxin.yn@gmail.com>
2017-12-12 11:41:12 -0800

    Fix the bug when subscribe button is clicked menu won't disappear.

--- themes/sosnpr/functions.php
+++ themes/sosnpr/functions.php
@@ -98,9 +98,12 @@ function sosnpr_scripts() {
 
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
+
+	wp_enqueue_script( 'smooth-scroll', get_template_directory_uri() . '/build/js/smooth-scroll.min.js', array('jquery'),null, true );
+
 	wp_enqueue_script( 'searchbar-toggle', get_template_directory_uri() . '/build/js/searchbar-toggle.min.js', array('jquery'), '20130115', true );
 
-	wp_enqueue_script( 'about-us-toggle', get_template_directory_uri() . '/build/js/about-us-toggle.min.js', array('jquery'),null, true );
+	wp_enqueue_script( 'about-us-toggle', get_template_directory_uri() . '/build/js/about-us-toggle.min.js', array('jquery', 'menu-toggle', 'smooth-scroll'),null, true );
 
 	wp_enqueue_script( 'mail-subscription', get_template_directory_uri() . '/build/js/mail-subscription.min.js', array('jquery'),null, true );
 
--- themes/sosnpr/js/menu-toggle.js
+++ themes/sosnpr/js/menu-toggle.js
@@ -1,39 +1,44 @@
 (function ($) {
   
-    $('.menu-item-106 a').on('click', function (e) {
-      e.preventDefault();
-  
-      
-  
-      
-    });
-    $('.menu-item-106').addClass('subscribe-button');
+  var clearMenu = false;
 
-    $('.menu-toggle').click(function() {
-      
-      
-          $('.menu1').toggleClass('menu-show');
-          // Remove the bind as it will be bound again on the next span click
-          $('.menu1').unbind('click');
-      
-     
-    
-    });
+
+  $('.menu-item-106 a').on('click', function (e) {
+    e.preventDefault();
+
+    clearMenu = false;
 
     
+  });
+
+  $('.menu-item-106').addClass('subscribe-button');
+
+  $('.menu-toggle').click(function(e) {
+    e.preventDefault();
     
-    $('.site-content').click(function(e) {
-      // If the click was not inside the active span
-      if(!$(e).hasClass("menu-show")) {
-          $('.menu1').removeClass('menu-show');
-          // Remove the bind as it will be bound again on the next span click
-          $('.menu1').unbind('click');
-      }
-      
-     
+    $('.menu1').addClass('menu-show');
+
+    clearMenu = false;
+  
+  });
+
     
-    });
-    
+  $('html').click(function() {
+
+    if(clearMenu){
+
+        $('.menu1').removeClass('menu-show');
+    }
+    clearMenu = true;
+
+  });
+
+$('.subscribe-button').click(function() {
+
+  $('.menu1').removeClass('menu-show');
+
+});
+
    
    
     
