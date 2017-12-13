e70c6ebd0ee9fffce502dc147b283106039e3adf
michaelujkim <michaelujkim@gmail.com>
2017-12-13 10:46:15 -0800

    formatted header for mobile and tablet fixed or not

--- themes/sosnpr/searchform.php
+++ themes/sosnpr/searchform.php
@@ -1,12 +1,14 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<!-- <fieldset> -->
-		<label>
-			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" style="padding-left:10px"/>
+		<label><a href="#">
+			<input type="search" class="search-field" placeholder="Search ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" style="padding-left:20px"/><i class="fa fa-times search-closing-icon" aria-hidden="true"  
+			></i></a>
 		</label>
 		<button class="search-submit">
 			<span class="icon-search" aria-hidden="true">
 				<i class="fa fa-search"></i>
 			</span>
+			
 			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
 		</button>
 	<!-- </fieldset> -->
