90c2208540d39b9d4b4e69061c7273a119d518d7
michaelujkim <michaelujkim@gmail.com>
2017-12-15 13:54:52 -0800

    deleted unnecessary alt text in resources

--- themes/sosnpr/page-templates/resources.php
+++ themes/sosnpr/page-templates/resources.php
@@ -7,7 +7,7 @@ get_header();
 ?>
 <div id="primary" class="content-area">
   <main id="main" class="site-main" role="main">
-  <header id="top" class="resources-hero" alt="mountain-landscape">
+  <header id="top" class="resources-hero">
   
      <?php while ( have_posts() ) : the_post(); ?>
 
