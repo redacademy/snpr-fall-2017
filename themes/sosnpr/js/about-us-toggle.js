5721a326b63935caf44b7e293d8ab28cd6b38df6
qingxin-yuan <qingxin.yn@gmail.com>
2017-12-16 13:46:57 -0800

    Fix the touch heading layout bug on about us page.

--- themes/sosnpr/js/about-us-toggle.js
+++ themes/sosnpr/js/about-us-toggle.js
@@ -5,22 +5,20 @@
     $(this).addClass('heading-click');
     $(this).parent().addClass('list-item-click');
     
-    $(this).siblings().find('.about-us-essay-hide').toggleClass('about-us-essay-show');
-    // $(this).parent().siblings().removeClass('list-item-click');
-    // $(this).parent().siblings().children().removeClass('heading-click');
-    // $(this).parent().siblings().find('.about-us-essay-hide').removeClass('about-us-essay-show');
-    
+    $(this).siblings().find('.about-us-essay-hide').addClass('about-us-essay-show');
+
   })
 
   $('.scroll-up').on('click touchstart', function(){
+
     $(this).siblings().removeClass('heading-click');
     $(this).siblings().find('.about-us-essay-hide').removeClass('about-us-essay-show');
     $(this).parent().removeClass('list-item-click');
     $(this).parent().siblings().removeClass('list-item-click');
     $(this).parent().siblings().children().removeClass('heading-click');
 
-    window.scrollTo(0,0);
-    // return true;
+    window.scrollTo(0,0); // scroll to the top
+
   })
 
 
