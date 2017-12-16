80626b989197d1f89ac2988609d2f87ded39b933
qingxin-yuan <qingxin.yn@gmail.com>
2017-12-16 14:16:38 -0800

    Fix the bug where in mobile could not make selection on the dropdown header menu.

--- themes/sosnpr/functions.php
+++ themes/sosnpr/functions.php
@@ -103,18 +103,17 @@ function sosnpr_scripts() {
 
 	wp_enqueue_script( 'feedback', get_template_directory_uri() . '/build/js/feedback.min.js', array('jquery'), null, true );
 
-	wp_enqueue_script( 'searchbar-toggle', get_template_directory_uri() . '/build/js/searchbar-toggle.min.js', array('jquery','smooth-scroll'), '20130115', true );
+	wp_enqueue_script( 'searchbar-toggle', get_template_directory_uri() . '/build/js/searchbar-toggle.min.js', array('jquery'), '20130115', true );
 
-	wp_enqueue_script( 'about-us-toggle', get_template_directory_uri() . '/build/js/about-us-toggle.min.js', array('jquery', 'menu-toggle', 'smooth-scroll'),null, true );
+	wp_enqueue_script( 'about-us-toggle', get_template_directory_uri() . '/build/js/about-us-toggle.min.js', array('jquery', 'smooth-scroll'),null, true );
 
-	wp_enqueue_script( 'resources-toggle', get_template_directory_uri() . '/build/js/resources-toggle.min.js', array('jquery'),null, true );	
+	wp_enqueue_script( 'resources-toggle', get_template_directory_uri() . '/build/js/resources-toggle.min.js', array('jquery'),null, true );
+
+	wp_enqueue_script( 'menu-toggle', get_template_directory_uri() . '/build/js/menu-toggle.min.js', array('jquery', 'owl-carousel'), false, true );
 
 	wp_enqueue_script( 'mail-subscription', get_template_directory_uri() . '/build/js/mail-subscription.min.js', array('jquery'),null, true );
 
-	wp_enqueue_script('owl-carousel', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js', array('jquery'), false, true );
-
-	
-	wp_enqueue_script( 'menu-toggle', get_template_directory_uri() . '/build/js/menu-toggle.min.js', array('jquery', 'owl-carousel'), false, true );
+	wp_enqueue_script('owl-carousel', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js', array(), false, true );
 
 	wp_enqueue_script( 'sosnpr-owl-carousel', get_template_directory_uri() . '/build/js/owl-carousel.min.js', array('jquery', 'owl-carousel'), false, true );
 
