3f9a1c0a755a81e86f886a2d49e832c51323f623
qingxin-yuan <qingxin.yn@gmail.com>
2017-12-16 14:44:16 -0800

    Finish the layout of scroll up in tablet and desktop on about us page.

--- themes/sosnpr/js/smooth-scroll.js
+++ themes/sosnpr/js/smooth-scroll.js
@@ -8,9 +8,9 @@
   .click(function(event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') 
       && 
-      location.hostname == this.hostname
+      location.hostname === this.hostname
     ) {
       // Figure out element to scroll to
       var target = $(this.hash);
