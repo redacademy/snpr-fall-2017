c800c88238c10c9e65fe28391f821b8e14a49470
qingxin-yuan <qingxin.yn@gmail.com>
2017-12-19 05:44:23 -0800

    Uniform the functionality in about us with proposed park page.

--- themes/sosnpr/js/about-us-toggle.js
+++ themes/sosnpr/js/about-us-toggle.js
@@ -6,6 +6,7 @@
     $(this).parent().addClass('list-item-click');
     
     $(this).siblings().find('.about-us-essay-hide').addClass('about-us-essay-show');
+    // $(this).parent().parent().find('.about-us-essay-hide').removeClass('list-item-click');
 
   })
 
@@ -14,11 +15,11 @@
     $(this).siblings().removeClass('heading-click');
     $(this).siblings().find('.about-us-essay-hide').removeClass('about-us-essay-show');
     $(this).parent().removeClass('list-item-click');
-    $(this).parent().siblings().removeClass('list-item-click');
-    $(this).parent().siblings().children().removeClass('heading-click');
+    // $(this).parent().siblings().removeClass('list-item-click');
+    // $(this).parent().siblings().children().removeClass('heading-click');
 
-    window.scrollTo(0,0); // scroll to the top
-
+    window.scrollTo(0,420); // scroll to the top
+  
   })
 
 
