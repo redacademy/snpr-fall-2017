b24e0939018828b9113c017afba303ec0f954685
qingxin-yuan <qingxin.yn@gmail.com>
2017-12-19 07:42:56 -0800

    reindent code in about us js

--- themes/sosnpr/js/about-us-toggle.js
+++ themes/sosnpr/js/about-us-toggle.js
@@ -1,38 +1,38 @@
 (function($){
   // var scroll = false;
-
+  
   $(".about-us-heading").on('click touchstart',function(event){
     event.preventDefault();
     
     // if (scroll){
     //   setTimeout(function(){
-        $(this).addClass('heading-click');
+    $(this).addClass('heading-click');
     $(this).parent().addClass('list-item-click');
     
     $(this).siblings().find('.about-us-essay-hide').addClass('about-us-essay-show');
-      // }, 1000);
+    // }, 1000);
     // }
     // scroll = true;
     // $(this).parent().parent().find('.about-us-essay-hide').removeClass('list-item-click');
-
+    
   })
-
+  
   $('.scroll-up').on('click touchstart', function(){
-
+    
     $(this).siblings().removeClass('heading-click');
     $(this).siblings().find('.about-us-essay-hide').removeClass('about-us-essay-show');
     $(this).parent().removeClass('list-item-click');
     // setTimeout(function(){
-      
+    
     // }, 1000);
     
     // scroll = true;
     // window.scrollTo(0,300); // scroll to the top
-  
-  
+    
+    
   })
-
-
+  
+  
 })(jQuery);
 
 
